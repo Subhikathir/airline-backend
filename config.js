515556e156a41c05d9e378ad2b50567048218cbf
Subhikathir <subhikshaphy1821@gmail.com>
2024-02-05 22:24:27 +0530

    mongo update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 //const MONGODB_URI = 'mongodb://localhost:27017/Air'; // Replace this with your actual MongoDB URI
-const MONGODB_URI = "mongodb+srv://subhiksha:sub123456@cluster0.o8gbsbf.mongodb.net/?retryWrites=true&w=majority";
+const MONGODB_URI = "mongodb+srv://subhikshaphy1821:subhikathir1@cluster0.um3tnyf.mongodb.net/?retryWrites=true&w=majority";
 module.exports = MONGODB_URI;
